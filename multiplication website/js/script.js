748cab16f53760579321fdd8ffee9f558c95053b
glessner264 <glessnermultimedia@gmail.com>
2013-03-06 16:18:51 -0500

    first project

---
+++ multiplication website/js/script.js
@@ -0,0 +1,104 @@
+// JavaScript Document
+
+/*whne show_only_2 is called, do what's in{}*/
+onload = init;
+function init(){
+	
+/*	document.getElementById("navhome").onclick=function()
+	document.getElementById("nav1").onclick=function()
+	document.getElementById("nav2").onclick=function()
+	document.getElementById("nav3").onclick=function()
+	document.getElementById("nav4").onclick=function()
+	
+}*/
+/*function show1(){
+	//console.log("show1");
+}
+*/
+
+function xshow_only_2(){
+/*make section 1 disappear*/
+	document.getElementById("section1").style.display="none";
+/*make section 2 appear*/
+	document.getElementById("section2").style.display="block";
+	
+	document.getElementById("section3").style.display="none";
+	
+	document.getElementById("section4").style.display="none";
+	
+	document.getElementById("section5").style.display="none";
+	
+	//document.getElementById("section6").style.display="none";
+}
+/*endsthe show_only_2 Function()*/
+
+
+function xshow_only_1(){
+/*make section 1 appear*/
+	document.getElementById("section1").style.display="block";
+/*make section 2 disappear*/
+	document.getElementById("section2").style.display="none";
+	
+	document.getElementById("section3").style.display="none";
+	
+	document.getElementById("section4").style.display="none";
+	
+	document.getElementById("section5").style.display="none";
+	
+	
+}
+/*endsthe show_only_2 Function()*/
+
+function xshow_only_3(){
+/*make section 1 appear*/
+	document.getElementById("section1").style.display="none";
+/*make section 2 disappear*/
+	document.getElementById("section2").style.display="none";
+	
+	document.getElementById("section3").style.display="block";
+	
+	document.getElementById("section4").style.display="none";
+	
+	document.getElementById("section5").style.display="none";
+	
+	
+}
+/*endsthe show_only_2 Function()*/
+
+
+
+
+function xshow_only_4(){
+/*make section 1 appear*/
+	document.getElementById("section1").style.display="none";
+/*make section 2 disappear*/
+	document.getElementById("section2").style.display="none";
+	
+	document.getElementById("section3").style.display="none";
+	
+	document.getElementById("section4").style.display="block";
+	
+	document.getElementById("section5").style.display="none";
+	
+
+}
+/*endsthe show_only_2 Function()*/
+
+
+function xshow_only_5(){
+/*make section 1 appear*/
+	document.getElementById("section1").style.display="none";
+/*make section 2 disappear*/
+	document.getElementById("section2").style.display="none";
+	
+	document.getElementById("section3").style.display="none";
+	
+	document.getElementById("section4").style.display="none";
+	
+	document.getElementById("section5").style.display="block";
+	
+}
+/*endsthe show_only_2 Function()*/
+}
+
+	
---
+++ multiplication website/js/val.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+function  Checkform(){
+	
+	if(document.getElementById("user_no").value==""){
+		document.getElementById("wrong_answer").innerHTML="* Please type the name of the dog.";
+		return false;
+		
+			} else {
+	
+	 return true;
+	}
+}
+
+
+
